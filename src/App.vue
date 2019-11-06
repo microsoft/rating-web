d6205f3b93ce0ecf64b6432596e5a9630912c781
Ahmed Sabbour <103856+sabbour@users.noreply.github.com>
2019-11-06 00:40:06 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,8 @@ export default {
 
 // main css
 body {
-  //background-color: #999;
-  background-color: #0071c5;
+  background-color: #999;
+  //background-color: #0071c5;
   line-height: 1;
 }
 
